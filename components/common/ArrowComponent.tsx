23a45252ad68b8935436d4461dd29d72aeb9fe96
8848Digital-Rohit-Frontend <rohit.g@8848digital.com>
2024-10-25 19:05:44 +0530

    feat: added common arrow component feat: rg

---
+++ components/common/ArrowComponent.tsx
@@ -0,0 +1,24 @@
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+export const LeftArrow = ({ handlePrev, disabled = false, ArrowLeft = FaArrowLeft, classname = '' }: any) => {
+  return (
+    <button className={classname} onClick={handlePrev} disabled={disabled}>
+      <ArrowLeft />
+    </button>
+  );
+};
+
+export const RightArrow = ({ handleNext, disabled = false, ArrowRight = FaArrowRight, classname = '' }: any) => {
+  return (
+    <button className={classname} onClick={handleNext} disabled={disabled}>
+      <ArrowRight />
+    </button>
+  );
+};
+
+export const CarouselArrows = ({ handlePrev, handleNext, disabledPrev, disabledNext }: any) => {
+  <div className="row justify-content-end mx-0" style={{ marginTop: '-50px', paddingBottom: '10px' }}>
+    <LeftArrow className="btn w-auto border-0" disabled={disabledPrev} handlePrev={handlePrev} ArrowLeft={FaArrowLeft} />
+    <RightArrow className="btn w-auto border-0" disabled={disabledNext} handleNext={handleNext} ArrowLeft={FaArrowRight} />
+  </div>;
+};
