a3e8dcf373551f3bdb3949f067f0f4cfca077bf5
8848Digital-Rohit-Frontend <rohit.g@8848digital.com>
2024-11-05 17:01:49 +0530

    feat: replace button validation added feat: rg

--- components/MyOrder/OrderCardDetails.tsx
+++ components/MyOrder/OrderCardDetails.tsx
@@ -178,7 +178,7 @@ function OrderCardDetails({ data, selectedMultiLangData }: any) {
                         </a>
                       </Link>
                     </button>
-                    {data?.order_status === 'Order Delivered' && (!data?.replacement_done || !data?.is_replacement) && (
+                    {data?.order_status === 'Order Delivered' && !data?.request_for_replacement && query?.status !== 'replaced-orders' && (
                       <button
                         className=" btn btn-link text-decoration-none mb-2 text-uppercase fs-14"
                         style={{ color: '#dc3545' }}
